66ebdc0096d94f74cce78b65b21ffb0e2949f8a0
h00kz <jlarrieu@student.42angouleme.fr>
2023-02-03 10:59:58 +0100

    oui

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -1,5 +1,5 @@
 #ifndef CUB3D_HPP
-#define CUB3D_HPP
+# define CUB3D_HPP
 
 # include <stdio.h>
 # include <stdlib.h>
@@ -8,6 +8,39 @@
 # include <fcntl.h>
 # include "../libft/libft.h"
 # include "../MLX42/include/MLX42/MLX42.h"
+
+// ----------------------- STRUCTS ------------------------- //
+typedef struct s_game t_game;
+typedef struct s_map t_map;
+typedef struct s_player t_player;
+typedef struct s_vec t_vec;
+typedef struct s_color t_color;
+
+typedef struct s_color
+{
+}	t_color;
+
+typedef struct s_vec
+{
+
+}	t_vec;
+
+typedef struct s_map
+{
+
+}	t_map;
+
+typedef struct s_player
+{
+
+}	t_player;
+
+
+typedef struct s_game
+{
+
+}	t_game;
+
 
 char	**parse(int argc, char **argv);
 void	ft_verif_map(char **map);
