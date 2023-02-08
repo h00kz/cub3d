f3be79cfbd6cda7a392b6ed5bed030c980993b4b
h00kz <jlarrieu@student.42angouleme.fr>
2023-02-08 17:58:11 +0100

    to

--- src/graphics/3d_projection.c
+++ src/graphics/3d_projection.c
@@ -8,9 +8,12 @@ int	average_color(int start_color, int end_color, float f)
 	int i;
 
 	i = 0;
-	while (i < 3)
+	if (f < 0 || f > 1)
+		return (start_color);
+	new_color = 0;
+	while (i < 4)
 	{
-		tmp = ((start_color >> 8 * i) & 0xff) * f + ((end_color >> 8 * i) & 0xff) * (1.0 - f);
+		tmp = ((start_color >> (8 * i)) & 0xff) * f + ((end_color >> (8 * i)) & 0xff) * (1.0 - f);
 		if (tmp > 255)
 			tmp = 0;
 		new_color += tmp << (i * 8);
@@ -23,14 +26,14 @@ void	render_3d(t_game *game)
 {
 	int		i;
 	int		y;
-	// int		color;
+	int		color;
 	float	perp_dist;
 	float	h_proj_wall;
 	int		wall_top_pixel;
 	int		wall_bottom_pixel;
 
 	i = 0;
-	// color = 0;
+	color = 0;
 	while (i < NB_RAYS)
 	{
 		perp_dist = game->player->rays[i].distance * cos(game->player->rays[i].ray_angle - game->player->rot_angle + TWO_PI);
@@ -41,23 +44,23 @@ void	render_3d(t_game *game)
 		wall_bottom_pixel = (WIN_HEIGHT / 2) + ((int)h_proj_wall / 2);
 		if (wall_bottom_pixel > WIN_HEIGHT)
 			wall_bottom_pixel = WIN_HEIGHT;
-		// if (game->player->rays[i].ray_face_up && !game->player->rays[i].was_hit_vertical)
-		// 	color = rgba2int(255, 36, 36, 255);
-		// else if (game->player->rays[i].ray_face_down && !game->player->rays[i].was_hit_vertical)
-		// 	color = rgba2int(255, 242, 46, 255);
-		// else if (game->player->rays[i].ray_face_right && game->player->rays[i].was_hit_vertical)
-		// 	color = rgba2int(0, 155, 5, 255);
-		// else if (game->player->rays[i].ray_face_left && game->player->rays[i].was_hit_vertical)
-		// 	color = rgba2int(201, 0, 0, 255);
+		if (game->player->rays[i].ray_face_up && !game->player->rays[i].was_hit_vertical)
+			color = rgba2int(255, 36, 36, 255);
+		else if (game->player->rays[i].ray_face_down && !game->player->rays[i].was_hit_vertical)
+			color = rgba2int(255, 242, 46, 255);
+		else if (game->player->rays[i].ray_face_right && game->player->rays[i].was_hit_vertical)
+			color = rgba2int(0, 155, 5, 255);
+		else if (game->player->rays[i].ray_face_left && game->player->rays[i].was_hit_vertical)
+			color = rgba2int(201, 0, 0, 255);
 		y = -1;
 		while (++y < wall_top_pixel)
-			put_pixel(game->mlx_img, i, y, rgba2int(game->map->floor->r,game->map->floor->g,game->map->floor->b, 255));
+			put_pixel(game->mlx_img, i, y,  average_color(rgba2int(0,0,0,255), rgba2int(game->map->floor->r,game->map->floor->g,game->map->floor->b, 255), perp_dist / 1500.0));
 		y = wall_top_pixel - 1;
 		while (++y < wall_bottom_pixel)
-			put_pixel(game->mlx_img, i, y , average_color(rgba2int(255,255,255,255), rgba2int(0,0,0,255), game->player->rays[i].distance / 5.0));
+			put_pixel(game->mlx_img, i, y , average_color(rgba2int(0,0,0,255), color, perp_dist / 1500.0));
 		y = WIN_HEIGHT + 1;
 		while (--y >= wall_bottom_pixel)
-			put_pixel(game->mlx_img, i, y, rgba2int(game->map->cell->r,game->map->cell->g,game->map->cell->b, 255));
+			put_pixel(game->mlx_img, i, y, average_color(rgba2int(0,0,0,255), rgba2int(game->map->cell->r,game->map->cell->g,game->map->cell->b, 255), perp_dist / 1500.0));
 		i++;
 	}
 }
