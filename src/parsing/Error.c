ff5208f0655bdc6e0b4798325b90703828553651
h00kz <jlarrieu@student.42angouleme.fr>
2023-02-15 15:00:14 +0100

    totu ouai

--- src/parsing/Error.c
+++ src/parsing/Error.c
@@ -43,34 +43,6 @@ void	ft_free2(t_game *game)
 	ft_free(game);
 }
 
-void	ft_error_next(int msg, t_game *game)
-{
-	if (msg == 4)
-	{
-		printf("Missing identifier\n");
-		ft_free2(game);
-	}
-	else if (msg == 5)
-	{
-		printf("Missing number(s) to make a color\n");
-		ft_free2(game);
-	}
-	else if (msg == 5)
-	{
-		printf("Invalid number(s) to make a color\n");
-		ft_free2(game);
-	}
-	else if (msg == 6)
-	{
-		printf("Missing a texture\n");
-		ft_free2(game);
-	}
-	else if (msg == 7)
-	{
-		printf("Invalid map\n");
-		ft_free2(game);
-	}
-}
 void	ft_free_split(char **strs)
 {
 	int	i;
